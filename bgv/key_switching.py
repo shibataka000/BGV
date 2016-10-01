c4e5ab428cc5bfa922f3e3fb4c88467a3e95d08d
shibataka000 <chise.alter.pasta@gmail.com>
2016-10-01 17:42:01 +0900

    Implement Powerof2.

--- bgv/key_switching.py
+++ bgv/key_switching.py
@@ -19,3 +19,12 @@ def bit_decomp(x, q):
         u = np.array([int(n) for n in u])
         u_list.append(u)
     return u_list
+
+
+def powerof2(x, q):
+    log_q = math.floor(math.log(q, 2))
+    x_list = []
+    for i in range(log_q + 1):
+        new_x = (2 ** i) * x
+        x_list.append(new_x)
+    return x_list
