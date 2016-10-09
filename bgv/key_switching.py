6902dae9c21b7a5f5f427a931e8e00c89a04725e
shibataka000 <chise.alter.pasta@gmail.com>
2016-10-09 23:02:29 +0900

    Implement "SwitchKeyGen" and "SwitchKey"

--- bgv/key_switching.py
+++ bgv/key_switching.py
@@ -3,6 +3,8 @@
 import math
 import numpy as np
 
+import bgv.basic_encryption_scheme as E
+
 
 def bit_decomp(x, q):
     def dec2bin(n, digit):
@@ -28,3 +30,21 @@ def powerof2(x, q):
         new_x = (2 ** i) * x
         x_list.append(new_x)
     return np.hstack(tuple(x_list))
+
+
+def switch_key_gen(s1, s2, params1, params2):
+    (q1, d1, n1, N1, chi1) = params1
+    (q2, d2, n2, N2, chi2) = params2
+    assert q1 == q2
+    q = q1
+    N2 = n1 * math.ceil(math.log(q, 2))
+    params2 = (q2, d2, n2, N2, chi2)
+
+    A = E.public_key_gen(params2, s2)
+    B = A + powerof2(s1, q)
+    return B
+
+
+def switch_key(tau, c1, q):
+    c2 = np.transpose(bit_decomp(c1, q)) * tau
+    return c2
