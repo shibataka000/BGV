65f9e3e62a7c173beaa6d7e77b46e80ca5d70acf
shibataka000 <chise.alter.pasta@gmail.com>
2016-12-10 22:34:18 +0900

    Fix bug in switch_key.

--- bgv/key_switching.py
+++ bgv/key_switching.py
@@ -38,13 +38,16 @@ def switch_key_gen(s1, s2, params1, params2):
     assert q1 == q2
     q = q1
     N2 = n1 * math.ceil(math.log(q, 2))
+    N2 = (n1 + 1) * math.ceil(math.log(q, 2))
     params2 = (q2, d2, n2, N2, chi2)
 
     A = E.public_key_gen(params2, s2)
-    B = A + powerof2(s1, q)
+    B = np.array(A)
+    B[:, 0] += powerof2(s1, q)
     return B
 
 
 def switch_key(tau, c1, q):
-    c2 = np.transpose(bit_decomp(c1, q)) * tau
+    c1t = np.transpose(bit_decomp(c1, q))
+    c2 = np.dot(c1t, tau)
     return c2
